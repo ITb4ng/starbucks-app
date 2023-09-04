3290827e5a4615a12988df6ea29c9811ca1df8aa
ITb4ng <bsh801099@gmail.com>
2023-09-04 23:15:29 +0900

    Autumn 업데이트

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ window.addEventListener('scroll', _.throttle(function () {
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
     gsap.to(fadeEl, 1, {
-        delay: (index + 1) * .7,
+        delay: (index + 1) * .5,
         opacity: 1
     });
 })
